354b478cf37049f185361281148048ba9f0326ff
Miguel Ruivo <miguel@miguelruivo.com>
2021-03-25 10:54:11 +0000

    Updates version constraints

--- lib/src/country_codes.dart
+++ lib/src/country_codes.dart
@@ -34,8 +34,8 @@ class CountryCodes {
   /// ```
   /// This will default to device's language if none is provided.
   static Future<bool> init([Locale? appLocale]) async {
-    final List<dynamic> locale = List<dynamic>.from(await (_channel.invokeMethod(
-            'getLocale', appLocale?.toLanguageTag())));
+    final List<dynamic> locale = List<dynamic>.from(
+        await (_channel.invokeMethod('getLocale', appLocale?.toLanguageTag())));
     if (locale != null) {
       _deviceLocale = Locale(locale[0], locale[1]);
       _localizedCountryNames = Map.from(locale[2]);
