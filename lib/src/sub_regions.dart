708a69a6d70ed4d99da4c84a9c00728cb471598c
Stefano Saitta <stefano.saitta92@gmail.com>
2024-11-08 14:25:28 +0100

    Support sub-region mapping to avoid crashes for non standard ISO country_codes

---
+++ lib/src/sub_regions.dart
@@ -0,0 +1,17 @@
+const Map<String, String> subRegionToCountryCode = {
+  "AC": "SH", // Ascension Island → Saint Helena
+  "AN": "NL", // Caribbean Netherlands  → Netherlands
+  "BU": "MM", // Burma → Myanmar
+  "CP": "FR", // Clipperton Island → France
+  "CS": "RS", // Serbia and Montenegro → Serbia
+  "DG": "IO", // Diego Garcia → British Indian Ocean Territory
+  "EA": "ES", // Ceuta and Melilla → Spain
+  "HV": "BF", // Upper Volta → Burkina Faso
+  "IC": "ES", // Canary Islands → Spain
+  "RH": "ZW", // Southern Rhodesia → Zimbabwe
+  "TA": "SH", // Tristan da Cunha → Saint Helena
+  "TP": "TL", // East Timor → Timor-Leste
+  "YD": "YE", // Democratic Yemen → Yemen
+  "YU": "RS", // Yugoslavia → Serbia
+  "ZR": "CD", // Zaire → Democratic Republic of the Congo
+};
